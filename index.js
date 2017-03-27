6803c571cf50830492c2333db810b9211eb5d4a7
Louis Center <git@louis.center>
2017-03-28 01:17:08 +1100

    Updated state

--- index.js
+++ index.js
@@ -1,51 +1,73 @@
-var html = require('choo/html')
 var choo = require('choo')
+var html = require('choo/html')
+var css = require('sheetify')
+
 var app = choo()
 
 app.use(function (state, emitter) {
+  // initial state
+  state.watch = ''
+  state.live = false
+  state.quality = 3
+  state.sources = {
+    available: { video: [], audio: [] },
+    selected: { video: null, audio: null }
+  }
 
-   state.live = false
-   state.quality = 3
-   state.sources = {
-     available: { video: [], audio: [] },
-     selected: { video: null, audio: null }
-   }
+  // toggle when starting/stopping broadcast
+  emitter.on('liveToggle', function (bool) {
+    state.live = bool
 
-   emitter.on('liveToggle', function (bool) {
-     state.live = bool
+    emitter.emit('render')
+  })
 
-     emitter.emit('render')
-   })
+  // sets broadcast bitrate
+  emitter.on('qualityToggle', function () {
+    var quality = state.quality
+    state.quality = (quality === 1) ? 3 : (quality - 1)
 
-   emitter.on('qualityToggle', function () {
-     var quality = state.quality
-     state.quality = (quality === 1) ? 3 : (quality - 1)
+    emitter.emit('render')
+  })
 
-     emitter.emit('render')
-   })
+  // sets available sources for broadcasting
+  emitter.on('sourcesAvailable', function (data) {
+    state.sources.available = {
+      video: data.video,
+      audio: data.audio
+    }
 
-   emitter.on('sourcesAvailable', function (data) {
-     state.sources.available = {
-       video: data.video,
-       audio: data.audio
-     }
+    emitter.emit('render')
+  })
 
-     emitter.emit('render')
-   })
+  // select broadcast sources
+  emitter.on('sourcesSelect', function (data) {
+    state.sources.selected = {
+      video: data.video,
+      audio: data.audio
+    }
 
-   emitter.on('sourcesSelect', function (data) {
-     state.sources.selected = {
-       video: data.video,
-       audio: data.video
-     }
+    emitter.emit('pushState', '/broadcast')
+  })
 
-     emitter.emit('render')
-   })
+  // watch stream
+  emitter.on('watch', function (data) {
+    state.watch = data
+  })
+
+  // redirect utility
+  emitter.on('redirect', function (data) {
+    emitter.emit('pushState', data)
+  })
 })
 
+// import base stylesheet
+css('./style.css')
+
+// routes
 app.route('/', require('./components/home'))
 app.route('/broadcast', require('./components/broadcast'))
 app.route('/view', require('./components/viewer'))
 app.route('/settings', require('./components/settings'))
 
+// start!
 document.body.appendChild(app.start())
